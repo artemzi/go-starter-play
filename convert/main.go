3d939cc602009e80b20b9102bdac7e0b45ed6613
Artem Zinoviev <arzinoviev@gmail.com>
2018-02-06 15:26:00 +0300

    imple convertor

---
+++ convert/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	inp := bufio.NewScanner(os.Stdin)
+	fmt.Println("Input integer for binary conversion:")
+	for inp.Scan() {
+		i, err := strconv.Atoi(inp.Text())
+		if err != nil {
+			log.Println("Wrong integer value in input")
+			os.Exit(2)
+		}
+		fmt.Printf("Bin value of input is: %b\n", i)
+	}
+}
