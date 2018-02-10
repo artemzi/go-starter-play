7f818cde68d9af245065a85320fdd54463420b51
Artem Zinoviev <arzinoviev@gmail.com>
2018-02-10 12:18:17 +0300

    save stage

---
+++ graph/main.go
@@ -0,0 +1,17 @@
+// there is no way to run it with `go run` for now.
+package graph
+
+var graph = make(map[string]map[string]bool)
+
+func addEdge(from, to string) {
+	edges := graph[from]
+	if edges == nil {
+		edges = make(map[string]bool)
+		graph[from] = edges
+	}
+	edges[to] = true
+}
+
+func hasEdge(from, to string) bool {
+	return graph[from][to]
+}
