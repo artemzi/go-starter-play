6e5d6dc6385d32adf2bd2d88734d56e9673530eb
Artem Zinoviev <arzinoviev@gmail.com>
2018-03-13 16:30:05 +0300

    problem initial setup

---
+++ leetcode/SmallestRotationWithHighestScore/main.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/contest/weekly-contest-75/problems/smallest-rotation-with-highest-score/
+package main
+
+import "fmt"
+
+func bestRotation(A []int) int {
+	return 1
+}
+
+func main() {
+	fmt.Print("Run with: go test -v\n")
+}
---
+++ leetcode/SmallestRotationWithHighestScore/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestBestRotation(t *testing.T) {
+	inp := []int{2, 3, 1, 4, 0}
+	res := bestRotation(inp)
+	// Explanation:
+	// Scores for each K are listed below:
+	// K = 0,  A = [2,3,1,4,0],    score 2
+	// K = 1,  A = [3,1,4,0,2],    score 3
+	// K = 2,  A = [1,4,0,2,3],    score 3
+	// K = 3,  A = [4,0,2,3,1],    score 4
+	// K = 4,  A = [0,2,3,1,4],    score 3
+
+	// For example, if we have [2, 4, 1, 3, 0], and we rotate by K = 2, it becomes [1, 3, 0, 2, 4].
+	// This is worth 3 points because 1 > 0 [no points], 3 > 1 [no points], 0 <= 2 [one point],
+	// 2 <= 3 [one point], 4 <= 4 [one point].
+
+	if res != 3 {
+		t.Errorf("Wrong result; got: %v, expected: 3.\n", res)
+	}
+}
