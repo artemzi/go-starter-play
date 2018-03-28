270a01698182d7772e2ac6cfa5c77e85aa882730
Artem Zinoviev <arzinoviev@gmail.com>
2018-03-28 10:24:15 +0300

    tests passed

--- hackerrank/insertionsort1/insertionsort1.go
+++ hackerrank/insertionsort1/insertionsort1.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"strings"
 )
 
-func insertionsort1(data *[]string) string {
+func insertionsort1(data []string, n int) string {
 	var result string
-	// for range *data {
-	// 	result += fmt.Sprintf("%v\n", strings.Join(*data, " "))
-	// }
+	i := n - 1
+	j := i
+	x := data[i]
+
+	for j > 0 && data[j-1] > x {
+		data[j] = data[j-1]
+		j--
+		result += fmt.Sprintf("%v\n", strings.Join(data, " "))
+	}
+	data[j] = x
+	result += fmt.Sprintf("%v\n", strings.Join(data, " "))
 	return result
 }
 
 func main() {
 	var l int
-	io := bufio.NewReader(os.Stdin)
-	fmt.Fscan(io, &l)
+	fmt.Scan(&l)
 
 	data := make([]string, l)
 	for i := 0; i < l; i++ {
-		fmt.Fscan(io, &data[i])
+		fmt.Scan(&data[i])
 	}
 
-	fmt.Printf("%v", insertionsort1(&data))
+	fmt.Printf("%v", insertionsort1(data, l))
 }
--- hackerrank/insertionsort1/insertionsort1_test.go
+++ hackerrank/insertionsort1/insertionsort1_test.go
@@ -11,8 +11,8 @@ import (
 
 func TestInsertionsort1(t *testing.T) {
 	data := []string{"2", "4", "6", "8", "3"}
-	expected := "2 4 6 8 8\n2 4 6 6 8\n2 4 4 6 8 \n2 3 4 6 8\n"
-	output := insertionsort1(&data)
+	expected := "2 4 6 8 8\n2 4 6 6 8\n2 4 4 6 8\n2 3 4 6 8\n"
+	output := insertionsort1(data, len(data))
 
 	assert.Equal(t, expected, output, "Wrong output")
 }
